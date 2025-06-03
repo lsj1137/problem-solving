b0b909bd8c0be615595d4c12c2fbd6a1815af666
Se Jun Lim <lsj1137jsl@gmail.com>
2025-06-03 18:07:33 +0900

    Update 유사 칸토어 비트열.js

--- 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.js
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.js
@@ -1,7 +1,6 @@
 function divideConquer(n, l, r) {
     let answer = 0;
     let unit = 5**(n-1);
-    // console.log(n,l,r);
     for (let i=0; i<5; i++) {
         let rangeSt = unit*i;
         let rangeFn = unit*(i+1)-1;
@@ -9,19 +8,15 @@ function divideConquer(n, l, r) {
             continue;
         }
         if (rangeSt < l && l <= rangeFn) {
-            // console.log("?", rangeSt, l, rangeFn);
             answer += divideConquer(n-1, l%unit, Math.min(rangeFn, r)%unit);
         } else if (l <= rangeSt && rangeFn <= r) {
             if (i!==2) {
                 answer += 4**(n-1);
             }
         } else if (rangeSt <= r && r <= rangeFn) {
-            // console.log("!", rangeSt, r, rangeFn);
             answer += divideConquer(n-1, Math.max(l, rangeSt)%unit, r%unit);
         }
     }
-    // console.log(answer);
-    // console.log("-----");
     return answer;
 }
 
@@ -30,7 +25,3 @@ function solution(n, l, r) {
     answer = divideConquer(n, l-1, r-1);
     return answer;
 }
-
-// 4 30 118 => 39
-// 3 1 125 => 64
-// 4 27 68 => 15
