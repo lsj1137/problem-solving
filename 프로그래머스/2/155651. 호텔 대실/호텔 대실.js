e17778aa57d4152d9a9318383e4e7161cebf3254
Se Jun Lim <lsj1137jsl@gmail.com>
2025-06-10 12:41:49 +0900

    [level 2] Title: 호텔 대실, Time: 24.15 ms, Memory: 38.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.js
@@ -0,0 +1,22 @@
+function toMinutes(time) {
+    let [hour,minute] = time.split(":").map((t)=>Number(t));
+    minute += hour*60;
+    return minute
+}
+
+function solution(book_time) {
+    var answer = 0;
+    let bs = toMinutes('00:00');
+    let be = toMinutes('24:10');
+    let check = Array.from(new Array(be), ()=>0);
+    for (usingTime of book_time) {
+        let start = toMinutes(usingTime[0]);
+        let end = toMinutes(usingTime[1])+10;
+        for (let i=start; i<end; i++) {
+            check[i] += 1;
+        }
+    }
+    // console.log(db);
+    answer = Math.max(...check);
+    return answer;
+}
