11e362ea60b9742efc82bd866ef07756eaaaa7ab
Se Jun Lim <lsj1137jsl@gmail.com>
2025-06-17 16:15:25 +0900

    [level 0] Title: 등수 매기기, Time: 0.78 ms, Memory: 88.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[][] score) {
+        int[] answer = new int[score.length];
+        Double[] avgs = new Double[score.length];
+        for (int i=0; i<score.length; i++) {
+            avgs[i] = (double)(score[i][0] + score[i][1])/(double)2;
+        }
+        Arrays.sort(avgs, Comparator.reverseOrder());
+        int index = 0;
+        for (int[] person: score) {
+            int rank = 0;
+            for (int i=0; i<avgs.length; i++) {
+                if ((double)(person[0] + person[1]) / (double)2 == avgs[i]) {
+                    rank = i+1;
+                    break;
+                }
+            }
+            answer[index++] = rank;
+        }
+        return answer;
+    }
+}
