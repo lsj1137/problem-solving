52812ff40c89f6d47164adf8c743fc362f9f6144
Se Jun Lim <lsj1137jsl@gmail.com>
2025-09-14 12:51:28 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,2 @@
+-- 코드를 작성해주세요
+select ID, EMAIL, FIRST_NAME, LAST_NAME from DEVELOPERS where SKILL_CODE & (select sum(CODE) from SKILLCODES where NAME in ('Python','C#')) in (select CODE from SKILLCODES where NAME in ('Python','C#')) or SKILL_CODE & (select sum(CODE) from SKILLCODES where NAME in ('Python','C#')) = (select sum(CODE) from SKILLCODES where NAME in ('Python','C#')) order by ID asc
